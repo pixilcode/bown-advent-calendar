bb7cd833c368508f1e682e0dcf6462a640504572
Brendon Bown <brendon.bown@gmail.com>
2025-05-25 15:42:54 -0600

    Convert days into pollen markup

---
+++ index.html.pm
@@ -0,0 +1,331 @@
+#lang pollen
+
+◊day[1]{
+  ◊para{
+    Each member of your family can give the Savior a special gift by trying to
+    live the law of love in some special way during this Christmas month.
+  }
+
+  ◊action{
+    Write what you are going to do to show love to Jesus on a piece of
+    paper and put it in a box. Then, wrap the box up and put it under the tree.
+    On Christmas Day, open the present and see if you gave the Savior a gift.
+  }
+}
+
+◊day[2]{
+  ◊question{What was the name of the angel that came to Joseph?}
+
+  ◊question{What house was Joseph from?}
+
+  ◊question{What does that mean to you?}
+
+  ◊scripture["Luke" 1 '(26 . 28)]
+}
+
+◊day[3]{
+  ◊question{Why did Mary go to Juda?}
+
+  ◊question{Who was Elizabeth?}
+
+  ◊question{What happened while Mary was visiting Elizabeth?}
+
+  ◊scripture["Luke" 1 '(39 . 45)]
+}
+
+◊day[4]{
+  ◊question{Why did Joseph Leave Galilee to go to Bethlehem?}
+
+  ◊question{How do you think Mary felt?}
+
+  ◊scripture["Luke" 2 '(1 . 7)]
+}
+
+◊day[5]{
+  ◊question{
+    The sign for Christ's birth was fulfilled. What was that sign for the
+    Nephites?
+  }
+
+  ◊scripture["Helaman" 14 '(3 . 6)]
+
+  ◊question{
+    Did you know that the reason you see so many candles at Christmas time is
+    because it is a symbol to remind you of the bright star at the birth of
+    Jesus?
+  }
+
+  ◊action{Light a candle and sing Silent Night.}
+}
+
+◊day[6]{
+  ◊action{Go and find a bell hanging somewhere in your house.}
+
+  ◊question{
+    Did you know the reason you see so many bells at Christmas is to remind you
+    of the sheep that came to the stable at the birth of Jesus?
+  }
+
+  ◊action{Maybe each of you would like to write a poem about bells.}
+}
+
+◊day[7]{
+  ◊question{What was the sign for the shepherds?}
+
+  ◊scripture["Luke" 2 '(8 . 20)]
+
+  ◊para{
+    Did you know the reason we see so many candy canes at Christmas is to remind
+    us of the shepherds that followed the star to find baby Jesus? A candy cane is
+    shaped like a shepherd's staff.
+  }
+
+  ◊action{Maybe everyone can draw some creative candy canes.}
+}
+
+◊day[8]{
+  ◊para{
+    The wise men did not come the night Jesus was born as the shepherds did. They
+    saw the star in the east and traveled far to see him. They arrived in
+    Palestine from the east sometime later, perhaps even months later.
+  }
+
+  ◊question{
+    When Herod sent the wise men to find Jesus, did they return to Herod? Where
+    did they go?
+  }
+
+  ◊scripture["Matthew" 2 '(1 . 12)]
+
+  ◊action{
+    The first person who answered the question correctly will find a treat under
+    the couch. Like the wise men, you have a choice to either share or keep your
+    treat. Which will it be?
+  }
+}
+
+◊day[9]{
+  ◊para{
+    There is no account that tells us just how many wise men came to worship the
+    Savior. There may have been three or maybe ten. But how many came is not as
+    important as that there were wise men who came to present their gifts and
+    worship this wondrous child. We today should remember that men who are wise
+    still seek him. If they prayerfully seek, they will find Him.
+  }
+
+  ◊action{Now go and seek under your pillow and ye shall find.}
+}
+
+◊day[10]{
+  ◊question{Can each member of your family give another name for Jesus?}
+
+  ◊scripture["Isaiah" 9 '(6 . 7)]
+}
+
+◊day[11]{
+  ◊para{
+    Have you ever wondered if Jesus really was the Savior? We all need to have
+    the seed of faith planted in our hearts. As we take care of it and it begins
+    to form, our faith fills our lives with blessings and we find miracles
+    happening in our lives. These may not be miracles like the moving of
+    mountains, but we will notice changes taking place in ourselves and those
+    around us that we thought would never happen. We will feel the blessings of
+    our Heavenly Father in our lives and have reason to rejoice. We will have
+    the confidence and strength to hold on to the iron rod until we reach
+    eternal life.
+  }
+
+  ◊action{Can you find a plant in your house that grew from a seed?}
+
+  ◊para{
+    That seed was planted just like your own seed of faith that must be planted
+    in your heart and then fed and nourished.
+  }
+}
+
+◊day[12]{
+  ◊question{
+    How did the Nephites who lived on the American continent know that Christ
+    would be born?
+  }
+
+  ◊scripture["Helaman" 14 '(1 . 2)]
+
+  ◊question{
+    How many years did Samuel the Lamanite prophesy would pass before the sign
+    of Christ's birth would be given?
+  }
+
+  ◊question{
+    Would you have believed Samuel the Lamanite? Did all the people believe him?
+  }
+
+  ◊scripture["3 Nephi" 1 '(4 . 9)]
+}
+
+◊day[13]{
+  ◊question{What did Jesus do when he was tempted?}
+
+  ◊scripture["Matthew" 4 '(2 . 10)]
+
+  ◊question{
+    Is there someone in the family that would like to share an experience they
+    have had when they were able to overcome a tempation?
+  }
+}
+
+◊day[14]{
+  ◊question{What did Christ do for the sick and handicapped?}
+
+  ◊scripture["Mark" 6 '(53 . 56)]
+
+  ◊scripture["Mark" 7 '(31 . 35)]
+
+  ◊action{
+    Now go and look in the refrigerator for a dessert you could share with
+    someone you know is not feeling on top of the world today.
+  }
+}
+
+◊day[15]{
+  ◊question{Was Christ kind? Give an example of his kindness.}
+
+  ◊action{Now ask your dad to leave the room. Is he gone?}
+
+  ◊para{
+    Your very own dad is a kind man who is trying to be as kind as the Savior.
+  }
+
+  ◊action{
+    What could you as a family do to show kindness to your dad? Keep this a
+    secret until tomorrow night, then surprise him with it.
+  }
+}
+
+◊day[16]{
+  ◊question{What did Christ do when someone offended Him?}
+
+  ◊scripture["Matthew" 27 '(29 . 31)]
+
+  ◊para{
+    Your very own bishop is a forgiving man. He understands that we are all
+    going to do things that are wrong and that we will regret later.
+  }
+
+  ◊question{
+    How could you as a family show your love to your bishop? Maybe you could all
+    write a letter or make a special treat and secretly deliver it to him
+    tonight.
+  }
+}
+
+◊day[17]{
+  ◊question{How did Christ feel towards those who were cruel to him?}
+
+  ◊scripture["Luke" 23 '(34 . #f)]
+
+  ◊action{
+    Choose a friend that has not always been kind to you. Now go look under your
+    pillow for a treat. Tomorrow share your treat with your friend at school or
+    wherever. Try not to let them know who gave it to them.
+  }
+}
+
+◊day[18]{
+  ◊para{
+    Make this month a time of tenderness, peace, and appreciation. By showing
+    love for each family member, not by the material gifts you give, but by the
+    way you treat and feel towards others.
+  }
+
+  ◊action{
+    Do a secret act of kindness for someone else in your family. Be sure to keep
+    it a secret.
+  }
+}
+
+◊day[19]{
+  ◊question{How did Jesus feel about truth?}
+
+  ◊scripture["John" 8 '(32 . #f)]
+
+  ◊para{
+    Your Primary teacher, Sunday School teacher, or Youth Leader is trying to
+    teach you many things that are true, just like the Savior did to those
+    around him.
+  }
+
+  ◊action{
+    How about giving your teacher a note or a special gift of appreciation?
+  }
+}
+
+◊day[20]{
+  ◊question{What did Jesus teach us about prayer?}
+
+  ◊scripture["Matthew" 6 '(5 . 13)]
+
+  ◊para{Your mom has taught you about prayer since the day you were born.}
+
+  ◊action{Ask your mom to leave the room for a moment. Is she gone?}
+
+  ◊action{
+    Can you think of a wonderful service you can do for your mom over the next
+    could of days? Try to make it a surprise.
+  }
+}
+
+◊day[21]{
+  ◊question{How did Christ feel about little children?}
+
+  ◊scripture["Mark" 10 '(13 . 16)]
+
+  ◊action{
+    Take time to read a story or play a game with someone younger in your family
+    or even a neighbor. Maybe a quick game as a family would be fun.
+  }
+}
+
+◊day[22]{
+  ◊question{Christ taught many things. What did he teach us about baptism?}
+
+  ◊scripture["Matthew" 3 '(13 . 17)]
+}
+
+◊day[23]{
+  ◊para{There is a surprise for you in the mailbox!}
+
+  ◊action{
+    After you are in your pajamas, choose someone to share the special story you
+    found in the mailbox. A cup of hot chocolate sounds good too.
+  }
+}
+
+◊day[24]{
+  ◊para{Tonight is a special night.}
+
+  ◊action{
+    Sing Christmas carols to a few neighbors or as a family around a lit candle.
+    Take time to sing everyone's favorite Christmas carol. If the time is
+    appropriate, have dad lead the family in a testimony sharing time. Discuss
+    how you have felt closer to Christ this month.
+  }
+}
+
+◊day[25]{
+  ◊action{
+    Remember to unwrap your present under the tree containing your special gift
+    you wanted to give Christ this Christmas season. Read and evaluate how you
+    did and what changes you made that you like and would want to cary on to the
+    New Year.
+  }
+
+  ◊action{
+    Try and record your thoughts and/or testimony in a journal to reflect on
+    through the years. Remember how much love Jesus has for all of His children.
+  }
+
+  ◊para{
+    Most of all, have a very Merry Christmas!
+  }
+}
